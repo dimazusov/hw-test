325c63220e5638aaa4cabd91f44b7ed73ce29469
Dmitry <dimazusov@yandex.ru>
2020-09-13 22:59:49 +0300

    change format

--- hw01_hello_now/go.mod
+++ hw01_hello_now/go.mod
@@ -4,7 +4,8 @@ go 1.14
 
 require (
 	bou.ke/monkey v1.0.2
-	github.com/beevik/ntp v0.2.0
+	github.com/beevik/ntp v0.3.0
 	github.com/stretchr/testify v1.4.0 // indirect
-	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
+	golang.org/x/sys v0.0.0-20200909081042-eff7692f9009 // indirect
 )
--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -9,14 +9,14 @@ import (
 )
 
 func main() {
-	currentTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	exactTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	const timeOutputFormat = "2006-01-02 15:04:05"
-	exactTime := time.Now()
+	const timeOutputFormat = "2006-01-02 15:04:05 +0000 UTC"
+	currentTime := time.Now()
 
-	fmt.Println("current time: ", currentTime.Format(timeOutputFormat))
-	fmt.Println("exact time: ", exactTime.Format(timeOutputFormat))
+	fmt.Println("current time:", currentTime.Format(timeOutputFormat))
+	fmt.Println("exact time:", exactTime.Format(timeOutputFormat))
 }
