c581afa6dc4a2345baebeb07b7a51d37bb6e4769
Dmitry <dimazusov@yandex.ru>
2020-09-13 21:06:44 +0300

    add homework 1

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	currentTime, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	const timeOutputFormat = "2006-01-02 15:04:05"
+	exactTime := time.Now()
+
+	fmt.Println("current time: ", currentTime.Format(timeOutputFormat))
+	fmt.Println("exact time: ", exactTime.Format(timeOutputFormat))
 }
