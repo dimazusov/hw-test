f561bc6dddb8ba747e7b78e380fc2501c49e771a
dimazusov <dimazusov@yandex.ru>
2020-11-21 20:09:57 +0300

    add hw07

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,62 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+const bufferSize = 2048
+
 func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
-	return nil
+	fromFile, err := os.Open(fromPath) // For read access.
+	if err != nil {
+		return err
+	}
+	defer fromFile.Close()
+
+	toFile, err := os.Create(toPath) // For read access.
+	if err != nil {
+		return err
+	}
+	defer toFile.Close()
+
+	_, err = fromFile.Seek(offset, 0)
+	if err != nil {
+		return ErrOffsetExceedsFileSize
+	}
+
+	countWritesBytes := int64(0)
+	for {
+		b := make([]byte, bufferSize)
+		n, err := fromFile.Read(b)
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+
+		if countWritesBytes+bufferSize > limit && limit != 0 {
+			_, err = toFile.Write(b[:countWritesBytes+limit])
+			if err != nil {
+				return err
+			}
+
+			return nil
+		}
+
+		if n != bufferSize {
+			b = b[:n]
+		}
+		_, err = toFile.Write(b)
+		if err != nil {
+			return err
+		}
+
+		countWritesBytes += int64(n)
+	}
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,95 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func isFileEqual(fromFilePath, toFilePath string) (isEqual bool, err error) {
+	fromFile, err := os.Open(fromFilePath) // For read access.
+	if err != nil {
+		return false, err
+	}
+	defer fromFile.Close()
+
+	toFile, err := os.Open(toFilePath) // For read access.
+	if err != nil {
+		return false, err
+	}
+	defer toFile.Close()
+
+	for {
+		expBuf := make([]byte, bufferSize)
+		givBuf := make([]byte, bufferSize)
+
+		expCount, fromFileErr := fromFile.Read(expBuf)
+		if fromFileErr != nil && fromFileErr != io.EOF {
+			return false, err
+		}
+
+		givCount, toFileErr := toFile.Read(givBuf)
+		if toFileErr != nil && toFileErr != io.EOF {
+			return false, err
+		}
+
+		if fromFileErr == io.EOF && toFileErr == io.EOF {
+			return true, nil
+		}
+
+		if expCount != givCount {
+			return false, nil
+		}
+
+		for i, _ := range expBuf {
+			if expBuf[i] != givBuf[i] {
+				return false, nil
+			}
+		}
+	}
+
+}
+
+type DataTestParams struct {
+	FromPath string
+	ToPath   string
+	Offset   int64
+	Limit    int64
+}
+
+func NewDataTestParams(fromPath, toPath string, offset, limit int64) *DataTestParams {
+	return &DataTestParams{
+		FromPath: fromPath,
+		ToPath:   toPath,
+		Offset:   offset,
+		Limit:    limit,
+	}
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here
+	fromFile := "./testdata/input.txt"
+	toFile := "./testdata/test_input.txt"
+
+	testParams := []*DataTestParams{
+		NewDataTestParams(fromFile, toFile, 0, 0),
+		NewDataTestParams(fromFile, toFile, 10, 1000),
+		NewDataTestParams(fromFile, toFile, 100, 200),
+		NewDataTestParams(fromFile, toFile, 213, 1),
+		NewDataTestParams(fromFile, toFile, 590, 3),
+		NewDataTestParams(fromFile, toFile, 600, 600),
+		NewDataTestParams(fromFile, toFile, 1553, 145),
+	}
+
+	for _, params := range testParams {
+		err := Copy(params.FromPath, params.ToPath, offset, limit)
+		require.Nil(t, err)
+
+		isEqual, err := isFileEqual(params.FromPath, params.ToPath)
+		require.Nil(t, err)
+		require.True(t, isEqual)
+	}
+
+	os.Remove(toFile)
 }
