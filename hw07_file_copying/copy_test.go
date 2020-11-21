ab9c1437b891498facdf06abe717b84a1ac9f3e9
dimazusov <dimazusov@yandex.ru>
2020-11-21 22:47:51 +0300

    fix

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -8,6 +8,49 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type DataTestParams struct {
+	FromPath     string
+	ToPath       string
+	ExpectedPath string
+	Offset       int64
+	Limit        int64
+}
+
+func NewDataTestParams(fromPath, toPath, expectedPath string, offset, limit int64) *DataTestParams {
+	return &DataTestParams{
+		FromPath:     fromPath,
+		ToPath:       toPath,
+		ExpectedPath: expectedPath,
+		Offset:       offset,
+		Limit:        limit,
+	}
+}
+
+func TestCopy(t *testing.T) {
+	fromFile := "./testdata/input.txt"
+	toFile := "./testdata/test_input.txt"
+
+	testParams := []*DataTestParams{
+		NewDataTestParams(fromFile, toFile, "./testdata/out_offset0_limit0.txt", 0, 0),
+		NewDataTestParams(fromFile, toFile, "./testdata/out_offset0_limit10.txt", 0, 10),
+		NewDataTestParams(fromFile, toFile, "./testdata/out_offset0_limit1000.txt", 0, 1000),
+		NewDataTestParams(fromFile, toFile, "./testdata/out_offset0_limit10000.txt", 0, 10000),
+		NewDataTestParams(fromFile, toFile, "./testdata/out_offset100_limit1000.txt", 100, 1000),
+		NewDataTestParams(fromFile, toFile, "./testdata/out_offset6000_limit1000.txt", 6000, 1000),
+	}
+
+	for _, params := range testParams {
+		err := Copy(params.FromPath, params.ToPath, params.Offset, params.Limit)
+		require.Nil(t, err)
+
+		isEqual, err := isFileEqual(params.ExpectedPath, params.ToPath)
+		require.Nil(t, err)
+		require.True(t, isEqual)
+	}
+
+	os.Remove(toFile)
+}
+
 func isFileEqual(fromFilePath, toFilePath string) (isEqual bool, err error) {
 	fromFile, err := os.Open(fromFilePath) // For read access.
 	if err != nil {
@@ -25,22 +68,18 @@ func isFileEqual(fromFilePath, toFilePath string) (isEqual bool, err error) {
 		expBuf := make([]byte, bufferSize)
 		givBuf := make([]byte, bufferSize)
 
-		expCount, fromFileErr := fromFile.Read(expBuf)
+		_, fromFileErr := fromFile.Read(expBuf)
 		if fromFileErr != nil && fromFileErr != io.EOF {
 			return false, err
 		}
 
-		givCount, toFileErr := toFile.Read(givBuf)
+		_, toFileErr := toFile.Read(givBuf)
 		if toFileErr != nil && toFileErr != io.EOF {
 			return false, err
 		}
 
 		if fromFileErr == io.EOF && toFileErr == io.EOF {
 			return true, nil
-		}
-
-		if expCount != givCount {
-			return false, nil
 		}
 
 		for i, _ := range expBuf {
@@ -50,46 +89,4 @@ func isFileEqual(fromFilePath, toFilePath string) (isEqual bool, err error) {
 		}
 	}
 
-}
-
-type DataTestParams struct {
-	FromPath string
-	ToPath   string
-	Offset   int64
-	Limit    int64
-}
-
-func NewDataTestParams(fromPath, toPath string, offset, limit int64) *DataTestParams {
-	return &DataTestParams{
-		FromPath: fromPath,
-		ToPath:   toPath,
-		Offset:   offset,
-		Limit:    limit,
-	}
-}
-
-func TestCopy(t *testing.T) {
-	fromFile := "./testdata/input.txt"
-	toFile := "./testdata/test_input.txt"
-
-	testParams := []*DataTestParams{
-		NewDataTestParams(fromFile, toFile, 0, 0),
-		NewDataTestParams(fromFile, toFile, 10, 1000),
-		NewDataTestParams(fromFile, toFile, 100, 200),
-		NewDataTestParams(fromFile, toFile, 213, 1),
-		NewDataTestParams(fromFile, toFile, 590, 3),
-		NewDataTestParams(fromFile, toFile, 600, 600),
-		NewDataTestParams(fromFile, toFile, 1553, 145),
-	}
-
-	for _, params := range testParams {
-		err := Copy(params.FromPath, params.ToPath, offset, limit)
-		require.Nil(t, err)
-
-		isEqual, err := isFileEqual(params.FromPath, params.ToPath)
-		require.Nil(t, err)
-		require.True(t, isEqual)
-	}
-
-	os.Remove(toFile)
 }
