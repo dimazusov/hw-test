47a492ed01a518392c5bc9a22657544e31dfbd29
dimazusov <dimazusov@yandex.ru>
2020-11-22 20:27:58 +0300

    add hw08

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,10 +1,47 @@
 package main
 
+import (
+	"bufio"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
 type Environment map[string]string
+
+const trimString = " \t"
+const tSymbols = "\x00"
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	env := Environment{}
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, f := range files {
+		file, err := os.Open(dir+"/"+f.Name())
+		if err != nil {
+			return nil, err
+		}
+
+		scanner := bufio.NewScanner(file)
+		ok := scanner.Scan()
+		if ok {
+			s := strings.Join(strings.Split(scanner.Text(), tSymbols), " ")
+			env[f.Name()] = strings.Trim(s, trimString)
+		} else {
+			env[f.Name()] = ""
+		}
+
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
+
+		file.Close()
+	}
+
+	return env, nil
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"errors"
+	"log"
+	"os"
+)
+
+var ErrCountArgs = errors.New("WrongCountArgs")
+
 func main() {
-	// Place your code here
+	if len(os.Args) < 2 {
+		log.Fatalln(ErrCountArgs)
+	}
+
+	env, err := ReadDir(os.Args[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	os.Exit(RunCmd(os.Args[2:], env))
 }
