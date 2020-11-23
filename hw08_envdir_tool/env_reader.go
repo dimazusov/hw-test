3d652673e116d608637a8a14dbd5cf5957c656ea
dimazusov <dimazusov@yandex.ru>
2020-11-23 20:07:25 +0300

    fix

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"os"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
 type Environment map[string]string
@@ -12,19 +14,23 @@ type Environment map[string]string
 const trimString = " \t"
 const tSymbols = "\x00"
 
+var ErrReading = errors.New("read error")
+var ErrOpen = errors.New("open error")
+var ErrScan = errors.New("scan error")
+
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
 	env := Environment{}
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, ErrReading)
 	}
 
 	for _, f := range files {
-		file, err := os.Open(dir+"/"+f.Name())
+		file, err := os.Open(dir + "/" + f.Name())
 		if err != nil {
-			return nil, err
+			return nil, errors.Wrap(err, ErrOpen)
 		}
 
 		scanner := bufio.NewScanner(file)
@@ -37,7 +43,7 @@ func ReadDir(dir string) (Environment, error) {
 		}
 
 		if err := scanner.Err(); err != nil {
-			return nil, err
+			return nil, errors.Wrap(err, ErrScan)
 		}
 
 		file.Close()
