7bb9936e86f7781ee378b360f53d7f665c0841da
dimazusov <dimazusov@yandex.ru>
2020-12-13 17:09:12 +0300

    add hw11

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,18 +1,87 @@
 package main
 
 import (
+	"bufio"
 	"io"
+	"net"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
 type TelnetClient interface {
-	// Place your code here
+	Connect() error
+	Close() error
+	Send() error
+	Receive() error
+}
+
+type tClient struct {
+	address string
+	timeout time.Duration
+	ln      net.Listener
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here
+	return &tClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (m *tClient) Connect() error {
+	var err error
+
+	m.conn, err = net.DialTimeout("tcp", m.address, m.timeout)
+	if err != nil {
+		return errors.Wrap(err, "cannot connect")
+	}
+
 	return nil
 }
 
-// Place your code here
-// P.S. Author's solution takes no more than 50 lines
+func (m *tClient) Close() error {
+	return m.conn.Close()
+}
+
+func (m *tClient) Send() error {
+	r := bufio.NewReader(m.in)
+	for {
+		b, _, err := r.ReadLine()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				return nil
+			}
+			return errors.Wrap(err, "cannot read")
+		}
+
+		b = append(b, '\n')
+		if _, err = m.conn.Write(b); err != nil {
+			return errors.Wrap(err, "cannot write")
+		}
+	}
+}
+
+func (m *tClient) Receive() error {
+	r := bufio.NewReader(m.conn)
+	for {
+		b, _, err := r.ReadLine()
+		if errors.Is(err, io.EOF) {
+			return nil
+		}
+		if err != nil {
+			return errors.Wrap(err, "cannot read")
+		}
+
+		b = append(b, '\n')
+		_, err = m.out.Write(b)
+		if err != nil {
+			return errors.Wrap(err, "cannot write")
+		}
+	}
+}
